b892bb06942707a44f8db30f65ca8bdb96382410
Son Nguyen <son.nguyen.hoang@quoine.com>
2020-09-03 00:42:14 +0700

    handling error

--- src/controllers/segwit.ts
+++ src/controllers/segwit.ts
@@ -4,7 +4,7 @@ import requestMiddleware from '../middleware/request-middleware';
 
 import * as AddressService from '../services/address-service';
 
-const create: RequestHandler = async (req, res) => {
+const create: RequestHandler = async (req, res, next) => {
   const { seed, path } = req.body;
   await check("seed", "Seed cannot be blank").not().isEmpty().run(req);
   await check("path", "Path cannot be blank").not().isEmpty().run(req);
@@ -12,14 +12,14 @@ const create: RequestHandler = async (req, res) => {
 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(500).json({ errors: errors.array() });
+    return next(errors);
   }
 
   try {
     const result = await AddressService.segwitFromSeedPath(seed, path)
     res.send(result);
   } catch (error) {
-    return res.status(500).json({ error });
+    return next(errors);
   }
 };
 
